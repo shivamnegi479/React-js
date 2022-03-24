bc4645c8b4fcc8f8039050accd450cb0a11d732c
shivam <shivamnegi479@gmail.com>
2022-03-24 20:39:06 +0530

    24 march first commit

---
+++ src/components/Alert.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Alert (props) {
+    const capitalize=(word)=>{
+        const lower=word.toLowerCase()
+        return lower.charAt[0].toUpperCase() +lower.slice(1)
+    }
+  return (
+    props.myalert &&<div>
+      <div className={`alert alert-${props.myalert.type} alert-dismissible fade show`} role="alert">
+        <strong>{capitalize(props.myalert.type)}</strong>: {props.myalert.msg}
+  <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+    </div>
+  )
+}
--- src/components/main.js
+++ src/components/main.js
@@ -1,34 +1,42 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 // import About from './About';
-import Navbar from './navbar';
-import TextForm from './TextForm'
-const Main =()=>{
-  const [mode, setmode] = useState('light');
-  const togllemode =()=>{
-    if(mode==="light"){
-
-      setmode("dark")
-      document.body.style.backgroundColor="#212529"
-      document.body.style.color="#fff"
-      document.getElementById('mybox').style.background="#212529"
-      document.getElementById('mybox').style.color="#fff"
-    }
-    else{
-      setmode('light')
-      document.body.style.backgroundColor="#fff"
-      document.body.style.color="#000"
-      document.getElementById('mybox').style.background="#fff"
-      document.getElementById('mybox').style.color="#000"
-    }
+import Alert from "./Alert";
+import Navbar from "./navbar";
+import TextForm from "./TextForm";
+const Main = () => {
+  const [mode, setmode] = useState("light");
+  const [alert, setAlert] = useState(null)
+  const showAlert= (message, type) =>{
+    setAlert({
+      msg:message,
+      type:type
+    })
   }
-  return(
+
+  const togllemode = () => {
+    if (mode === "light") {
+      setmode("dark");
+      document.body.style.backgroundColor = "#212529";
+      document.body.style.color = "#fff";
+      document.getElementById("mybox").style.background = "#212529";
+      document.getElementById("mybox").style.color = "#fff";
+      showAlert('Dark mode has been enabled','success')
+    } else {
+      setmode("light");
+      document.body.style.backgroundColor = "#fff";
+      document.body.style.color = "#000";
+      document.getElementById("mybox").style.background = "#fff";
+      document.getElementById("mybox").style.color = "#000";
+      showAlert('Light mode has been enabled','success')
+    }
+  };
+  return (
     <>
-    <Navbar  title="MyWebs" mode={mode} toggleMode={togllemode}/>
-    <TextForm heading="Enter the text to analyze" />
-   
-    {/* < About /> */}
- 
-   </>
-  )
-}
+      <Navbar title="MyWebs" mode={mode} toggleMode={togllemode} />
+      <Alert myalert={alert} />
+      <TextForm heading="Enter the text to analyze" />
+      {/* < About /> */}
+    </>
+  );
+};
 export default Main;
